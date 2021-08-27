89992258d199b394747f8500e2c70cf027299723
micasa-acerman <koha009@yandex.ru>
2021-08-27 09:09:03 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ const App = () => {
           width="100%"
           src="https://sun9-6.userapi.com/impf/dew2JIgMNVnJKycJYdgvGw_QiGFu9cfrnkaU4A/VDINGXg8qJ0.jpg?size=490x379&quality=96&sign=9a4e45e06205977ced3460bb0b709e28&type=album"
         />
-        <Title level={3}>как вам понравилось первое занятие?</Title>
-        <Paragraph>И что Вы хотели бы улучшить.</Paragraph>
+        <Title level={3}>Вам понравилось первое занятие?</Title>
+        <Paragraph>Что Вы хотели бы улучшить:</Paragraph>
         <TextArea showCount maxLength={100} />
       </div>
     </Layout>

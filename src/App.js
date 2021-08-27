8105e6c65805e24a1a422e4f880e58f42a36b8c0
micasa-acerman <koha009@yandex.ru>
2021-08-27 09:43:18 +0300

    4 files modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,29 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import './style.css';
+import React, { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
 export default function App() {
-  const [value, setValue] = useState('');
-  const [result, setResult] = useState();
+  const classes = useStyles();
+  const [isOpen, setOpen] = useState(false);
+  const [title, setTitle] = useState('Inbox');
+  const handleClickMenuItem = text => {
+    setTitle(text);
+    setOpen(false);
+  };
 
-  useEffect(() => {
-    const cast = Number(value);
-    if (cast && cast >= value && cast === Math.round(cast)) {
-      let sum = 0;
-      for (let i = 1; i <= value; i++) {
-        sum += i;
-      }
-      setResult(sum);
-    } else setResult('Введите целое положительное число');
-  }, [value]);
   return (
-    <div class="form">
-      <label for="inputN">N</label>
-      <input
-        id="inputN"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-      <span>{result}</span>
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+          onClick={() => setOpen(true)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          {title}
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+      <Drawer open={isOpen} onClose={() => setOpen(false)}>
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem
+              button
+              key={text}
+              onClick={handleClickMenuItem.bind(null, text)}
+            >
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem
+              button
+              key={text}
+              onClick={handleClickMenuItem.bind(null, text)}
+            >
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </AppBar>
   );
 }

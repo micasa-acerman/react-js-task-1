baebc3f67e16f6ed744ba6c5cf008a00d63d3c32
micasa-acerman <koha009@yandex.ru>
2021-08-29 15:47:07 +0300

    2 files modified, 10 added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './style.css';
+import Blog from './components/Blog.js';
 
-export default function App() {
-  const [value, setValue] = useState('');
-  const [result, setResult] = useState();
-
-  useEffect(() => {
-    const cast = Number(value);
-    if (cast && cast >= value && cast === Math.round(cast)) {
-      let sum = 0;
-      for (let i = 1; i <= value; i++) {
-        sum += i;
-      }
-      setResult(sum);
-    } else setResult('Введите целое положительное число');
-  }, [value]);
-  return (
-    <div class="form">
-      <label for="inputN">N</label>
-      <input
-        id="inputN"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-      <span>{result}</span>
-    </div>
-  );
+export default function App(props) {
+  return <Blog />;
 }

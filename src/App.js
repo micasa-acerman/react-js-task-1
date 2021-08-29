dd34b8252bd182e62d35d9bdcaecb4f3e7577ae9
micasa-acerman <koha009@yandex.ru>
2021-08-29 16:36:49 +0300

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import './style.css';
+import './App.css';
+
+const posts = [
+  {
+    id: '1',
+    title: 'First post',
+    description: 'Lorem ipsum'
+  },
+  {
+    id: '2',
+    title: 'Second post',
+    description: 'Lorem ipsum'
+  },
+  {
+    id: '3',
+    title: 'Third post',
+    description: 'Lorem ipsum'
+  }
+];
 
 export default function App() {
-  const [value, setValue] = useState('');
-  const [result, setResult] = useState();
+  const [selectedPostId, setPostId] = useState();
 
-  useEffect(() => {
-    const cast = Number(value);
-    if (cast && cast >= value && cast === Math.round(cast)) {
-      let sum = 0;
-      for (let i = 1; i <= value; i++) {
-        sum += i;
-      }
-      setResult(sum);
-    } else setResult('Введите целое положительное число');
-  }, [value]);
   return (
-    <div class="form">
-      <label for="inputN">N</label>
-      <input
-        id="inputN"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-      <span>{result}</span>
-    </div>
+    <ul className="posts">
+      {posts
+        .filter(p => (selectedPostId ? p.id == selectedPostId : true))
+        .map(post => (
+          <div
+            key={post.id}
+            className={`item ${selectedPostId === post.id ? 'active' : ''}`}
+            onClick={() => setPostId(selectedPostId ? null : post.id)}
+          >
+            <h3>{post.title}</h3>
+            <p>{post.description}</p>
+          </div>
+        ))}
+    </ul>
   );
 }

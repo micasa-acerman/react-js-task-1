57682c7472fd02038a9dcacc53f69413c4a034e5
micasa-acerman <koha009@yandex.ru>
2021-08-29 15:16:55 +0300

    1 file modified

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 const renderElement = () =>
   ReactDOM.render(
     <div className="container">
-      <h2>Задание 3</h2>
+      <h2>Задание 2</h2>
       <p>Текущее время: {new Date().toLocaleTimeString()}</p>
     </div>,
     document.getElementById('root')

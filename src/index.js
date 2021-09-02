45c60bca725d668be502f0f48c77de522a14b02b
micasa-acerman <koha009@yandex.ru>
2021-09-02 21:57:37 +0300

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,54 +1,15 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const employee = [
-  {
-    firstName: 'Alan',
-    lastName: 'Rayder',
-    workHours: 16,
-    hourlyWageRate: 10
-  },
-  {
-    firstName: 'Big',
-    lastName: 'Smoke',
-    workHours: 40,
-    hourlyWageRate: 2
-  },
-  {
-    firstName: 'Van',
-    lastName: 'Hoenhaim',
-    workHours: 5,
-    hourlyWageRate: 2
-  },
-  {
-    firstName: 'Albus',
-    lastName: 'Dumbledore',
-    workHours: 16,
-    hourlyWageRate: 10
-  }
-];
+function Hello() {
+  const [name, setName] = useState('Ivan');
+  const [show, setShow] = useState(false);
 
-function EmployeeTable() {
   return (
-    <table border="1">
-      <tr>
-        <th>First name</th>
-        <th>Last name</th>
-        <th>Work Hours</th>
-        <th>Hourly wage rate</th>
-        <th>Salary</th>
-      </tr>
-      {employee.map(e => (
-        <tr key={e.firstName}>
-          <td>{e.firstName}</td>
-          <td>{e.lastName}</td>
-          <td>{e.workHours}</td>
-          <td>{e.hourlyWageRate} $</td>
-          <td>{e.workHours * e.hourlyWageRate} $</td>
-        </tr>
-      ))}
-    </table>
+    <div>
+      {show ? 'Привет' : 'Пока'} {name}
+    </div>
   );
 }
 
-ReactDOM.render(<EmployeeTable />, document.getElementById('root'));
+ReactDOM.render(<Hello />, document.getElementById('root'));

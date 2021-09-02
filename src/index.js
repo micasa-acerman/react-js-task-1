7f2269a053bfa3a0bd9bbbb96d1e603cd274fe8b
micasa-acerman <koha009@yandex.ru>
2021-09-02 21:54:25 +0300

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,76 +1,54 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class Products extends Component {
-  state = {
-    products: []
-  };
-
-  componentDidMount() {
-    this.setState({
-      products: [
-        { id: 1, name: 'Товар 1', price: 1000 },
-        { id: 2, name: 'Товар 2', price: 2000 },
-        { id: 3, name: 'Товар 3', price: 3000 },
-        { id: 4, name: 'Товар 4', price: 4000 },
-        { id: 5, name: 'Товар 5', price: 5000 }
-      ]
-    });
+const employee = [
+  {
+    firstName: 'Alan',
+    lastName: 'Rayder',
+    workHours: 16,
+    hourlyWageRate: 10
+  },
+  {
+    firstName: 'Big',
+    lastName: 'Smoke',
+    workHours: 40,
+    hourlyWageRate: 2
+  },
+  {
+    firstName: 'Van',
+    lastName: 'Hoenhaim',
+    workHours: 5,
+    hourlyWageRate: 2
+  },
+  {
+    firstName: 'Albus',
+    lastName: 'Dumbledore',
+    workHours: 16,
+    hourlyWageRate: 10
   }
+];
 
-  create = (id, name, price) => {
-    this.setState({
-      products: [...this.state.products, { id, name, price }]
-    });
-  };
-
-  update = (id, name, price) => {
-    this.setState(
-      this.state.products.map(p => ({
-        id,
-        name: id === p.id ? name : p.name,
-        price: id === p.id ? price : p.price
-      }))
-    );
-  };
-
-  delete = tid => {
-    this.setState({
-      products: this.state.products.filter(({ id }) => id !== tid)
-    });
-  };
-
-  render() {
-    return (
-      <table className="products">
-        <thead>
-          <tr>
-            <td className="products__th">#</td>
-            <td className="products__th">Название</td>
-            <td className="products__th">Цена</td>
-            <td className="products__th">Действие</td>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.products.map(({ id, name, price }) => (
-            <tr key={String(id)}>
-              <td className="products__tr">{id}</td>
-              <td className="products__tr">{name}</td>
-              <td className="products__tr">{price}</td>
-              <td className="products__tr">
-                <button
-                  className="products__btn"
-                  onClick={() => this.delete(id)}
-                >
-                  Удалить
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
+function EmployeeTable() {
+  return (
+    <table border="1">
+      <tr>
+        <th>First name</th>
+        <th>Last name</th>
+        <th>Work Hours</th>
+        <th>Hourly wage rate</th>
+        <th>Salary</th>
+      </tr>
+      {employee.map(e => (
+        <tr key={e.firstName}>
+          <td>{e.firstName}</td>
+          <td>{e.lastName}</td>
+          <td>{e.workHours}</td>
+          <td>{e.hourlyWageRate} $</td>
+          <td>{e.workHours * e.hourlyWageRate} $</td>
+        </tr>
+      ))}
+    </table>
+  );
 }
 
-ReactDOM.render(<Products />, document.getElementById('root'));
+ReactDOM.render(<EmployeeTable />, document.getElementById('root'));
